cc531d23e30cc38d032ab9f8757772a3906adebc
M-Hatlem <markushatl1999@gmail.com>
2020-02-27 15:17:38 +0100

    fixed html cleaner and line spaces errors

--- Lift_data.py
+++ Lift_data.py
@@ -1,5 +1,7 @@
+# requires request BeautifulSoup4, download at: pip install beautifulsoup4
 import json, re
 from rdflib import URIRef, Graph, Namespace, Literal
+from bs4 import BeautifulSoup
 
 # This function lifts the data downloaded from nav into rdf triples
 def lift_data():
@@ -20,8 +22,11 @@ def lift_data():
                     graph.add( (nameSpace[unique_key], nameSpace[graph_predicate], nameSpace[graph_predicate + "-unique_key-" + unique_key]) )
                     job_data_dict = job_ad[graph_predicate]
                     for dict_data_from_dict in job_data_dict:
-                        graph.add( (nameSpace[unique_key], nameSpace[dict_data_from_dict], Literal(job_data_dict[dict_data_from_dict])) )
-                elif graph_predicate == "description":
+                        if dict_data_from_dict == "description" and job_data_dict[dict_data_from_dict] is not None:
+                            graph.add( (nameSpace[graph_predicate + "-unique_key-" + unique_key], nameSpace[dict_data_from_dict], Literal(clean_html_tag(job_data_dict[dict_data_from_dict]))) )
+                        else:
+                            graph.add( (nameSpace[graph_predicate + "-unique_key-" + unique_key], nameSpace[dict_data_from_dict], Literal(job_data_dict[dict_data_from_dict])) )
+                elif graph_predicate == "description" and job_ad[graph_predicate] is not None:
                     graph.add((nameSpace[unique_key], nameSpace[graph_predicate], Literal(clean_html_tag(job_ad[graph_predicate]))) )
                 elif type(job_ad[graph_predicate]) is str:
                     graph.add((nameSpace[unique_key], nameSpace[graph_predicate], nameSpace[clean_spaces(job_ad[graph_predicate])]) )
@@ -33,21 +38,22 @@ def lift_data():
 
 # This function removes html tags from the description of the ads
 def clean_html_tag(in_data):
-    cleaner = re.compile('<.*?>')
-    in_data = re.sub(cleaner, '', in_data)
-    out_data = re.sub('\n', '', in_data)
+    soup=BeautifulSoup(in_data, 'html.parser')
+    out_data=soup.get_text()
+    out_data = out_data.replace("\r","")
+    out_data = out_data.replace("\n", "")
     return out_data
 
 def clean_spaces(in_data):
-    out_data = re.sub(' ', '_', in_data)
+    in_data = re.sub(' ', '_', in_data)
+    in_data = re.sub("'", '', in_data)
+    out_data = re.sub('"', '', in_data)
     return out_data
 
 
 if __name__ == "__main__":
     lift_data()
 
-# TODO: clean html line gaps from input with regexp in function
-
 # TODO: Replace example with out own ontology
 
-# TODO: create a fucntion that imports replaces ex in schema and other vocab properties in the fniished file
+# TODO: create a fucntion that imports replaces ex in schema and other vocab properties in the finished file
