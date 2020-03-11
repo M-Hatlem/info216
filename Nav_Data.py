5ce509a420d42db07ae095e8a81a7ff5c123ee98
M-Hatlem <markushatl1999@gmail.com>
2020-03-11 15:04:33 +0100

    test2

--- Nav_Data.py
+++ Nav_Data.py
@@ -136,6 +136,10 @@ class NavData:
         for row in qres:
             print("%s has title %s" % row)   # TODO example print, change to display in GUI
 
+    def test(self):
+        x = 1
+        #delete this
+
 
 if __name__ == "__main__":
     api_public_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwdWJsaWMudG9rZW4udjFAbmF2Lm5vIiwiYXVkIjoiZmVlZC1hcGktdjEiLCJpc3MiOiJuYXYubm8iLCJpYXQiOjE1NTc0NzM0MjJ9.jNGlLUF9HxoHo5JrQNMkweLj_91bgk97ZebLdfx3_UQ'
