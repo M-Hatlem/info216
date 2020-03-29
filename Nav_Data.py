6b7e3bcd7e08c00ea055cc6fa95bb1f2b77dc00b
M-Hatlem <markushatl1999@gmail.com>
2020-03-29 15:36:58 +0200

    added a gui with context menus and options to change token, save and import files. Query mode still has to be done, search does not work yet

--- Nav_Data.py
+++ Nav_Data.py
@@ -7,37 +7,40 @@ import re
 from rdflib import Graph, Namespace, Literal
 from rdflib.namespace import XSD
 from bs4 import BeautifulSoup
+import tkinter
 
 
+#This class handles all the data and functionality related to it such as downloading it, semnatically annotating and lifting it as well as allowing us to run queries
 class NavData:
     def __init__(self, token):
         self.token = token
         self.graph = Graph()
         self.data = None
 
-    # allows you to update nav's download token
+    # allows you to update Nav's download token in case it's expired or has been changed
     def update_token(self, new_token):
         self.token = new_token
 
     # This function downloads the data set from Nav, it requires a token as an input
     def download_data(self):
+        interface.status.set("Downloading data please wait...")
+        interface.gui.update_idletasks()
         api_endpoint = 'https://arbeidsplassen.nav.no/public-feed/api/v1/ads?size=100'
         api_headers = {'accept': 'application/json',
                        'Authorization': 'Bearer ' + self.token}
         download = requests.get(url=api_endpoint, headers=api_headers)
         if download.status_code == 200:
             self.data = download.json()
-            print("Download complete")  # TODO example print, change to display in GUI
             self.lift_data()
             return True
         elif download.status_code == 401:
-            print("Error 401 not authorized, public token likely expired. Get a new one at: https://github.com/navikt/pam-public-feed")  # TODO example print, change to display in GUI
+            interface.status.set("Error 401 not authorized, public token likely expired \n go to settings --> 'update Token' and input a new one, then press Re-download in the settings menu to update. \n Get a new token at: https://github.com/navikt/pam-public-feed")
             return False
         else:
-            print("Error: " + str(download.status_code))  # TODO example print, change to display in GUI
+            interface.status.set(" http error: " + str(download.status_code) + "\n program failed try again")
             return False
 
-    # if you want to save the json data downloaded, do this after a download, requires filname as input:
+    # if you want to save the json data downloaded, do this after a download, requires filename as input:
     def save_json(self, filename):
         with open(filename, 'w', encoding='utf-8') as f:
             json.dump(self.data, f, ensure_ascii=False, indent=4)
@@ -50,6 +53,8 @@ class NavData:
 
     # This function lifts the data downloaded from nav into rdf triples
     def lift_data(self):
+        interface.status.set("Download complete, lifting data please wait...:")
+        interface.gui.update_idletasks()
         ns = Namespace("http://example.org/")  # TODO example properties, remember to change, Replace ns example with out own ontology
         self.graph.bind("ex", ns)
         sch = Namespace("http://schema.org/")
@@ -63,11 +68,11 @@ class NavData:
                     self.graph.add((ns[unique_key], ns[graph_predicate], Literal(graph_predicate + "-uid:" + unique_key)))
                     for dict_data_from_list in job_ad[graph_predicate]:
                         for elm_in_list in dict_data_from_list:
-                            if (elm_in_list == "country" or elm_in_list == "county" or elm_in_list == "city") and elm_in_list is not None:
+                            if (elm_in_list == "country" or elm_in_list == "county" or elm_in_list == "city") and dict_data_from_list[elm_in_list] is not None:
                                 self.graph.add((ns[graph_predicate + "-uid:" + unique_key], dbp[elm_in_list], ns[self.clean_text(dict_data_from_list[elm_in_list])]))
                             elif elm_in_list == "postalCode":
                                 self.graph.add((ns[graph_predicate + "-uid:" + unique_key], dbp[elm_in_list], Literal(dict_data_from_list[elm_in_list])))
-                            elif elm_in_list == "municipal":
+                            elif elm_in_list == "municipal" and dict_data_from_list[elm_in_list] is not None:
                                 self.graph.add((ns[graph_predicate + "-uid:" + unique_key], dbp["municipality"], ns[self.clean_text(dict_data_from_list[elm_in_list])]))
                             elif elm_in_list == "address":
                                 self.graph.add((ns[graph_predicate + "-uid:" + unique_key], sch[elm_in_list], Literal(dict_data_from_list[elm_in_list])))
@@ -79,19 +84,19 @@ class NavData:
                     for dict_data_from_dict in job_data_dict:
                         if dict_data_from_dict == "description" and job_data_dict[dict_data_from_dict] is not None:
                             self.graph.add((ns[graph_predicate + "-uid:" + unique_key], sch[dict_data_from_dict], Literal(self.clean_html_tags(job_data_dict[dict_data_from_dict]))))
-                        elif dict_data_from_dict == "name" and job_ad[graph_predicate] is not None:
+                        elif dict_data_from_dict == "name" and job_data_dict[dict_data_from_dict] is not None:
                             self.graph.add((ns[graph_predicate + "-uid:" + unique_key], sch[dict_data_from_dict], ns[self.clean_text(job_data_dict[dict_data_from_dict])]))
-                        elif dict_data_from_dict == "homepage" and job_ad[graph_predicate] is not None:
+                        elif dict_data_from_dict == "homepage":
                             self.graph.add((ns[graph_predicate + "-uid:" + unique_key], sch["url"], Literal(job_data_dict[dict_data_from_dict])))
-                        elif dict_data_from_dict == "orgnr" and job_ad[graph_predicate] is not None:
+                        elif dict_data_from_dict == "orgnr":
                             self.graph.add((ns[graph_predicate + "-uid:" + unique_key], sch["identifier"], Literal(job_data_dict[dict_data_from_dict])))
                         else:
                             self.graph.add((ns[graph_predicate + "-uid:" + unique_key], ns[dict_data_from_dict], Literal(job_data_dict[dict_data_from_dict])))
                 elif graph_predicate == "description" and job_ad[graph_predicate] is not None:
                     self.graph.add((ns[unique_key], sch[graph_predicate], Literal(self.clean_html_tags(job_ad[graph_predicate]))))
-                elif graph_predicate == "updated" and job_ad[graph_predicate] is not None:
+                elif graph_predicate == "updated":
                     self.graph.add((ns[unique_key], ns[graph_predicate], Literal(job_ad[graph_predicate], datatype=XSD.datetime)))
-                elif (graph_predicate == "link" or graph_predicate == "sourceurl" or graph_predicate == "positioncount") and job_ad[graph_predicate] is not None:
+                elif graph_predicate == "link" or graph_predicate == "sourceurl" or graph_predicate == "positioncount":
                     self.graph.add((ns[unique_key], ns[graph_predicate], Literal(job_ad[graph_predicate])))
                 elif graph_predicate == "title" and job_ad[graph_predicate] is not None:
                     self.graph.add((ns[unique_key], sch[graph_predicate], ns[self.clean_text(job_ad[graph_predicate])]))
@@ -99,21 +104,24 @@ class NavData:
                     self.graph.add((ns[unique_key], sch["jobTitle"], ns[self.clean_text(job_ad[graph_predicate])]))
                 elif graph_predicate == "engagementtype" and job_ad[graph_predicate] is not None:
                     self.graph.add((ns[unique_key], sch["employmentType"], ns[self.clean_text(job_ad[graph_predicate])]))
-                elif graph_predicate == "expires" and job_ad[graph_predicate] is not None:
+                elif graph_predicate == "expires":
                     self.graph.add((ns[unique_key], sch[graph_predicate], Literal(job_ad[graph_predicate], datatype=XSD.datetime)))
-                elif graph_predicate == "starttime" and job_ad[graph_predicate] is not None:
+                elif graph_predicate == "starttime":
                     self.graph.add((ns[unique_key], sch["jobStartDate"], Literal(job_ad[graph_predicate], datatype=XSD.datetime)))
-                elif graph_predicate == "applicationDue" and job_ad[graph_predicate] is not None:
+                elif graph_predicate == "applicationDue":
                     self.graph.add((ns[unique_key], sch["applicationDeadline"], Literal(job_ad[graph_predicate], datatype=XSD.datetime)))
-                elif graph_predicate == "published" and job_ad[graph_predicate] is not None:
+                elif graph_predicate == "published":
                     self.graph.add((ns[unique_key], sch["datePosted"], Literal(job_ad[graph_predicate], datatype=XSD.datetime)))
-                elif graph_predicate == "link" and job_ad[graph_predicate] is not None:
+                elif graph_predicate == "link":
                     self.graph.add((ns[unique_key], sch["relatedLink"], Literal(job_ad[graph_predicate])))
                 elif type(job_ad[graph_predicate]) is str and job_ad[graph_predicate] is not None:
                     self.graph.add((ns[unique_key], ns[graph_predicate], ns[self.clean_text(job_ad[graph_predicate])]))
                 else:
                     self.graph.add((ns[unique_key], ns[graph_predicate], Literal(job_ad[graph_predicate])))
             #break  # To test run on only one ad remove # on #break
+        interface.status.set("Lifting complete")
+        interface.gui.update_idletasks()
+        interface.query_mode()
 
     # This function removes html tags and new lines from the description of the ads
     @staticmethod
@@ -124,7 +132,7 @@ class NavData:
         out_data = out_data.replace("\n", "")
         return out_data
 
-    # This function removes new lines, ' and " from the description of the ads
+    # This function removes new lines, ' and " from the strings to make them URL friendly
     @staticmethod
     def clean_text(in_data):
         in_data = re.sub(' ', '_', in_data)
@@ -139,26 +147,74 @@ class NavData:
     # This function allows you to load a turtle file you previously serialized into the graph, requires the name of the file to load
     def load_serialized_data(self, filename):
         self.graph.parse(filename, format="turtle")
+        interface.query_mode()
 
-    # Runs allows us to run queries on the graph
+    # Runs queries on the graph
     def query(self, statement):
         qres = self.graph.query(statement)
         for row in qres:
             print("%s has title %s" % row)   # TODO example print, change to display in GUI
 
 
+# This class controls the GUI and everything displayed to the user
+class TKinterGui:
+    # This sets up the essentials of the GUI and adds functionality to the context menu
+    def __init__(self):
+        self.q_mode_active = False
+        self.gui = tkinter.Tk()
+        self.gui.title('Semantic job-searcher')
+        self.gui.geometry("800x500")
+        menu = tkinter.Menu(self.gui)
+        self.gui.config(menu=menu)
+        importmenu = tkinter.Menu(self.gui)
+        menu.add_cascade(label="Import file", menu=importmenu)
+        importmenu.add_command(label='Import JSON file', command=lambda: nav.load_json("data.json"))
+        importmenu.add_command(label='Import Turtle file', command=lambda: nav.load_serialized_data('nav_triples.ttl'))
+        savemenu = tkinter.Menu(self.gui)
+        menu.add_cascade(label="Save data", menu=savemenu)
+        savemenu.add_command(label='Save JSON file', command=lambda: nav.save_json("data.json"))
+        savemenu.add_command(label='Save Turtle file', command=lambda: nav.serialize('nav_triples.ttl'))
+        settingsmenu = tkinter.Menu(self.gui)
+        menu.add_cascade(label="Settings", menu=settingsmenu)
+        settingsmenu.add_command(label='Update token', command=self.update_token)
+        settingsmenu.add_command(label='Re-download data', command=nav.download_data)
+        self.status = tkinter.StringVar()
+        tkinter.Label(self.gui, textvariable=self.status).place(relx=.5, rely=.3, anchor="center")
+        download_btn = tkinter.Button(text='Download data sets', command=lambda: [download_btn.place_forget(), nav.download_data()])
+        download_btn.place(relx=.5, rely=.5, anchor="center")
+
+    #Opens the page for updating your token, this can only be done if you have't entered query mode already as you already need valid data to do so. Will already show the latest token used by us, but can be changed
+    def update_token(self):
+        if self.q_mode_active is True:
+            pass
+        else:
+            self.status.set("Public token:")
+            input_token_fld = tkinter.Entry(self.gui, width=100)
+            input_token_fld.place(relx=.5, rely=.3, anchor="center")
+            input_token_fld.insert(0, api_public_token)
+            input_token_btn = tkinter.Button(text='Set token', command=lambda: [self.status.set(""), input_token_fld.place_forget(), input_token_btn.place_forget(),  nav.update_token(input_token_fld.get())])
+            input_token_btn.place(relx=.5, rely=.4, anchor="center")
+
+    #This function allows the users to run queries and sets up the GUI accordingly
+    def query_mode(self):
+        self.status.set("")
+        interface.gui.update_idletasks()
+        self.q_mode_active = True
+        tkinter.Label(self.gui, text="Search:").pack()
+        search_fld = tkinter.Entry(self.gui, width=50)
+        search_fld.pack()
+        search_fld.insert(0, api_public_token)
+        search_btn = tkinter.Button(text='Search', command=lambda: nav.query(example_query))
+        search_btn.pack()
+
+
 if __name__ == "__main__":
     api_public_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwdWJsaWMudG9rZW4udjFAbmF2Lm5vIiwiYXVkIjoiZmVlZC1hcGktdjEiLCJpc3MiOiJuYXYubm8iLCJpYXQiOjE1NTc0NzM0MjJ9.jNGlLUF9HxoHo5JrQNMkweLj_91bgk97ZebLdfx3_UQ'
-    example_query = """PREFIX ex:<http://example.org/> SELECT DISTINCT ?uuid ?title WHERE {?uuid ex:title ?title .}"""
+    example_query = """SELECT DISTINCT ?uuid ?title WHERE {?uuid schema:title ?title .}"""
     nav = NavData(api_public_token)
-    #nav.update_token(api_public_token)
-    nav.download_data()
-    #nav.save_json("data.json")
-    #nav.load_json("data.json")
-    nav.serialize('nav_triples.ttl')
-    #nav.load_serialized_data('nav_triples.ttl')
-    nav.query(example_query)
-    # TODO Create GUI with support for all functions
+    interface = TKinterGui()
+    interface.gui.mainloop()
+    # TODO finish gui query mode
     # TODO Add Dbpedia integration for linking to info about cities/countries/etc.
     # TODO improve and expand query and filtering options
     # TODO add option to search on UIB's sv faculty study lines and get possible jobs for a student with x degree
