5b3b036cafa6203612c93a56e19eb1224bd12116
M-Hatlem <markushatl1999@gmail.com>
2020-04-17 18:08:03 +0200

    The program now downloads all of the nav data and not just the 100 newest ads

--- Nav_Data.py
+++ Nav_Data.py
@@ -1,16 +1,18 @@
 # requires request library, download at: pip install requests
 # requires BeautifulSoup4, download at: pip install beautifulsoup4
 # requires rdflib, download at: pip install rdflib
+# requires jsonmerge, download at: pip install jsonmerge
 import requests
 import json
 import re
+from jsonmerge import Merger
 from rdflib import Graph, Namespace, Literal
 from rdflib.namespace import XSD
 from bs4 import BeautifulSoup
 import tkinter
 
 
-#This class handles all the data and functionality related to it such as downloading it, semnatically annotating and lifting it as well as allowing us to run queries
+# This class handles all the data and functionality related to it such as downloading it, semnatically annotating and lifting it as well as allowing us to run queries
 class NavData:
     def __init__(self, token):
         self.token = token
@@ -23,22 +25,33 @@ class NavData:
 
     # This function downloads the data set from Nav, it requires a token as an input
     def download_data(self):
-        interface.status.set("Downloading data please wait...")
+        counter = 0
+        interface.status.set("Downloading data please wait..." + " \n Data sets downloaded: " + str(counter))
         interface.gui.update_idletasks()
         api_endpoint = 'https://arbeidsplassen.nav.no/public-feed/api/v1/ads?size=100'
-        api_headers = {'accept': 'application/json',
-                       'Authorization': 'Bearer ' + self.token}
+        api_headers = {'accept': 'application/json', 'Authorization': 'Bearer ' + self.token}
+        merg_schema = {"properties": {"content": {"mergeStrategy": "append"}}}
+        merger = Merger(merg_schema)
         download = requests.get(url=api_endpoint, headers=api_headers)
         if download.status_code == 200:
             self.data = download.json()
+            download_active = True
+            while download_active is True:
+                latest_download = (self.data['content'][len(self.data['content']) - 1]['published'])[0:19]
+                download = requests.get(url=api_endpoint + "&published=[*," + latest_download + ")", headers=api_headers)
+                counter += 1
+                interface.status.set("Downloading data please wait..." + " \n Data sets downloaded: " + str(counter) + " \n Total job ads fetched: " + str(len(self.data['content'])))
+                interface.gui.update_idletasks()
+                self.data = merger.merge(self.data, download.json())
+                # if counter == 2:   # Dev option to not download all ads, but instead a limited number, makes for faster testing. remove the # on this and the line bellow to use
+                #    download_active = False
+                if latest_download == (self.data['content'][len(self.data['content']) - 1]['published'])[0:19]:
+                    download_active = False
             self.lift_data()
-            return True
         elif download.status_code == 401:
             interface.status.set("Error 401 not authorized, public token likely expired \n go to settings --> 'update Token' and input a new one, then press Re-download in the settings menu to update. \n Get a new token at: https://github.com/navikt/pam-public-feed")
-            return False
         else:
             interface.status.set(" http error: " + str(download.status_code) + "\n program failed try again")
-            return False
 
     # if you want to save the json data downloaded, do this after a download, requires filename as input:
     def save_json(self, filename):
@@ -118,7 +131,7 @@ class NavData:
                     self.graph.add((ns[unique_key], ns[graph_predicate], ns[self.clean_text(job_ad[graph_predicate])]))
                 else:
                     self.graph.add((ns[unique_key], ns[graph_predicate], Literal(job_ad[graph_predicate])))
-            #break  # To test run on only one ad remove # on #break
+            # break  # Dev option to test run on only one ad remove # on #break
         interface.status.set("Lifting complete")
         interface.gui.update_idletasks()
         interface.query_mode()
@@ -135,9 +148,7 @@ class NavData:
     # This function removes new lines, ' and " from the strings to make them URL friendly
     @staticmethod
     def clean_text(in_data):
-        in_data = re.sub(' ', '_', in_data)
-        in_data = re.sub("'", '', in_data)
-        out_data = re.sub('"', '', in_data)
+        out_data = re.sub('[^A-Za-z0-9_]+', '', in_data)
         return out_data
 
     # This function serializes the data to a turtle file and saves it locally. Requires a name to save as
@@ -190,7 +201,7 @@ class TKinterGui:
         download_btn = tkinter.Button(text='Download data sets', command=lambda: [download_btn.place_forget(), nav.download_data()])
         download_btn.place(relx=.5, rely=.5, anchor="center")
 
-    #Opens the page for updating your token, this can only be done if you have't entered query mode already as you already need valid data to do so. Will already show the latest token used by us, but can be changed
+    # Opens the page for updating your token, this can only be done if you have't entered query mode already as you already need valid data to do so. Will already show the latest token used by us, but can be changed
     def update_token(self):
         if self.q_mode_active is True:
             pass
@@ -202,7 +213,7 @@ class TKinterGui:
             input_token_btn = tkinter.Button(text='Set token', command=lambda: [self.status.set(""), input_token_fld.place_forget(), input_token_btn.place_forget(),  nav.update_token(input_token_fld.get())])
             input_token_btn.place(relx=.5, rely=.4, anchor="center")
 
-    #This function allows the users to run queries and sets up the GUI accordingly
+    # This function allows the users to run queries and sets up the GUI accordingly
     def query_mode(self):
         self.status.set("")
         interface.gui.update_idletasks()
